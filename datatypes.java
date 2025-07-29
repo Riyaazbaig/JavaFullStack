c0f5633d4b27b3af8ce4fbbdd5e76e32781a4712
Riyaazbaig <riyaazbaig@gmail.com>
2025-07-29 15:49:41 +0530

    Day 2

---
+++ datatypes.java
@@ -0,0 +1,25 @@
+package day2;
+
+public class datatypes {
+	public static void main(String args[]) {
+		//short = 32767
+		//byte = 127
+		//int = we can store upto 10 numbers (eg : 1234567890)
+		//long = any number but at the end we have to put l (eg : a=4448841l)
+		
+		//float = we get round number eg 33.3
+		//double = we the the actual number eg 22.5151
+		
+		//boolean = true or false 
+		 short a=10;
+		 short b=90;
+		 System.out.println(a+b);
+		
+		 //operators +,-,*,/,%
+		 //operands = a,b 
+		 
+		 
+		 
+	}
+
+}
